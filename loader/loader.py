d1017ee3521cc2ca63fe35dab760e8d6f43bc989
lesson10_homework <FrendOfMan@yandex.ru>
2022-05-14 13:50:57 +0300

    домашка

---
+++ loader/loader.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, request, render_template
+
+from functions import load_posts, uploads_posts
+import logging
+
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
+
+loader_blueprint = Blueprint('loader', __name__, url_prefix='/post', static_folder='static', template_folder='templates')
+
+@loader_blueprint.route('/form/')
+def form():
+    return render_template('post_form.html')
+
+@loader_blueprint.route('/upload/', methods=["POST"])
+def upload():
+    try:
+       file = request.files['picture']
+       filename = file.filename
+       content = request.values['content']
+       posts = load_posts()
+       posts.append({
+           'pic': f'/uploads/images/{filename}',
+           'content': content
+       })
+       uploads_posts(posts)
+       file.save(f'uploads/images/{filename}')
+       if filename.split('_')[-1] not in ['png', 'jpeg', 'jpg']:
+           logging.info('Файл не является изображением')
+    except FileNotFoundError:
+        logging.error('Ошибка при загрузке файла')
+        return "<h1> файл не найден </h1>"
+    else:
+        return render_template('post_uploaded.html', pic=f'/uploads/images/{filename}', content=content)
+
+#    posts = [x for x in load_posts() if search_by.lower() in x['content'].lower()]
+#    return render_template('post_list.html', search_by=search_by, posts=posts)
